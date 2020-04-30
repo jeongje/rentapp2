f1a048c4627a6e3b5008f97d1c531c9bad9c3ed7
unknown <uonnow@gmail.com>
2020-04-30 09:10:12 +0900

    입주자, 계약, 입금내역 sidebar외형 완성

--- src/code.js
+++ src/code.js
@@ -1,11 +1,44 @@
-function showSidebar() {
+function onOpen(e) {
+
+    SpreadsheetApp.getUi()
+        .createMenu("입력 도구")
+        .addItem("입주자 입력", "tenantSidebar")
+        .addItem("계약 입력", "contractSidebar")
+        .addItem("입금내역 입력", "paymentSidebar")
+        .addToUi();
+
+}
+
+
+
+function tenantSidebar() {
 
     let template = HtmlService.createTemplateFromFile("tenant");
-
     let html = template.evaluate();
-
     html.setTitle("입주자 입력")
 
     SpreadsheetApp.getUi().showSidebar(html);
-  
+
+}
+
+
+function contractSidebar() {
+
+    let template = HtmlService.createTemplateFromFile("contract");
+    let html = template.evaluate();
+    html.setTitle("계약 입력")
+
+    SpreadsheetApp.getUi().showSidebar(html);
+
+}
+
+
+function paymentSidebar() {
+
+    let template = HtmlService.createTemplateFromFile("payment");
+    let html = template.evaluate();
+    html.setTitle("입금내역 입력")
+
+    SpreadsheetApp.getUi().showSidebar(html);
+
 }
