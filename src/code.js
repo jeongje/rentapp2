35f6ffc680183d586da92aa1c4b94f8ff3235315
unknown <uonnow@gmail.com>
2020-04-30 09:37:37 +0900

    건물 sidebar추가, id수정

--- src/code.js
+++ src/code.js
@@ -1,3 +1,6 @@
+const ss = SpreadsheetApp.getActiveSpreadsheet()
+
+
 function onOpen(e) {
 
     SpreadsheetApp.getUi()
@@ -5,40 +8,9 @@ function onOpen(e) {
         .addItem("입주자 입력", "tenantSidebar")
         .addItem("계약 입력", "contractSidebar")
         .addItem("입금내역 입력", "paymentSidebar")
+        .addItem("건물 입력", "buildingSidebar")
         .addToUi();
 
 }
 
 
-
-function tenantSidebar() {
-
-    let template = HtmlService.createTemplateFromFile("tenant");
-    let html = template.evaluate();
-    html.setTitle("입주자 입력")
-
-    SpreadsheetApp.getUi().showSidebar(html);
-
-}
-
-
-function contractSidebar() {
-
-    let template = HtmlService.createTemplateFromFile("contract");
-    let html = template.evaluate();
-    html.setTitle("계약 입력")
-
-    SpreadsheetApp.getUi().showSidebar(html);
-
-}
-
-
-function paymentSidebar() {
-
-    let template = HtmlService.createTemplateFromFile("payment");
-    let html = template.evaluate();
-    html.setTitle("입금내역 입력")
-
-    SpreadsheetApp.getUi().showSidebar(html);
-
-}
